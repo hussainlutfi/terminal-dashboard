f322d35238c4365310b56e90be5b70e47521c0d4
7ussainlz <hussainlutfi@hotmail.com>
2024-07-09 11:31:01 +0300

    title

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       dir="rtl"
       className={`flex min-h-screen min-w-full ${tajawal.className} font-sans`}
     >
-      <div className="w-full bg-gradient-to-br from-[#51170e] to-[#031020] ">
+      <div className="w-full bg-gradient-to-br from-[#51170e] to-[#031020]  ">
         <div className="flex flex-col items-start justify-center pt-16 sm:pt-16">
           <button
             onClick={() => {
